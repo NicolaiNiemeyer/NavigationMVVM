55551492e2b00a52093c23b8f2e1213c937c9451
NicolaiNiemeyer <97104254+NicolaiNiemeyer@users.noreply.github.com>
2022-03-09 14:51:25 +0100

    Add project files.

---
+++ ViewModels/AccountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+  public class AccountViewModel : ViewModelBase
+  {
+    public string Name => "SingletonSean";
+
+    public ICommand NavigateHomeCommand { get; }
+
+    public AccountViewModel(NavigationStore navigationStore)
+    {
+      NavigateHomeCommand = new NavigateCommand<HomeViewModel>(
+        navigationStore,
+        () => new HomeViewModel(navigationStore));
+    }
+  }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+  public class HomeViewModel : ViewModelBase
+  {
+    public string WelcomeMessage => "Welcome to my app";
+
+    public ICommand NavigateAccountCommand { get; }
+
+    public HomeViewModel(NavigationStore navigationStore)
+    {
+      NavigateAccountCommand = new NavigateCommand<AccountViewModel>(
+        navigationStore,
+        () => new AccountViewModel(navigationStore));
+    }
+  }
+}
