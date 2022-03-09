ed0e46407c2f1dfc08a3c8d3972d62273b52f006
NicolaiNiemeyer <97104254+NicolaiNiemeyer@users.noreply.github.com>
2022-03-09 14:59:37 +0100

    Started implementing login function

---
+++ Commands/LoginCommand.cs
@@ -0,0 +1,20 @@
+﻿using NavigationMVVM.Stores;
+using NavigationMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationMVVM.Commands
+{
+  public class LoginCommand : CommandBase
+  {
+    private readonly LoginViewModel _viewModel;
+    private readonly NavigationStore _navigationStore;
+    public override void Execute(object parameter)
+    {
+      
+    }
+  }
+}
